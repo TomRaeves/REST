9b164309f05d73c43bf7d1e2bc7415372fd92042
tomra <tom.raeves3we@gmail.com>
2021-04-25 16:45:22 +0200

    adding joint account

--- src/main/java/com/company/restservice/Account.java
+++ src/main/java/com/company/restservice/Account.java
@@ -3,6 +3,7 @@ package com.company.restservice;
 public class Account {
     private float balance;
     private int accountNmbr;
+    private boolean busy=false;
 
     public Account(int accountNmbr){
         this.balance = 0;
@@ -18,8 +19,18 @@ public class Account {
     }
 
     public boolean getMoney(float money){
+        if(busy)
+            return false;
+            //somebody is already getting money from this account at this moment
+
+        busy = true;
         if(balance >= money) {
             balance -= money;
+
+        try{ Thread.sleep(5000);
+        }catch (Exception e){}
+
+        busy = false;
             return true;
         } else {return false;}
     }
---
+++ src/main/java/com/company/restservice/jointAccount.java
@@ -0,0 +1,4 @@
+package com.company.restservice;
+
+public class jointAccount {
+}
